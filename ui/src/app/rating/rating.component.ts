090267865173fdfe818c9b51a39dd3236a527154
mohamedirfan03 <m.irfan@bankcbw.org>
2020-09-28 23:04:40 +0530

    minnor changes

--- ui/src/app/rating/rating.component.ts
+++ ui/src/app/rating/rating.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { DatePipe } from '@angular/common';
 import { AppService } from '../app.service';
 import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
+import { ToastrService } from 'ngx-toastr';
 
 
 @Component({
@@ -20,7 +21,7 @@ export class RatingComponent implements OnInit {
     value: any;
     yesterday: any;
     rollno: any;
-    constructor(private datePipe: DatePipe, public appService: AppService, private dialog: MatDialog) {
+    constructor(private datePipe: DatePipe, public toaster: ToastrService, public appService: AppService, private dialog: MatDialog) {
 
 
     }
@@ -51,27 +52,31 @@ export class RatingComponent implements OnInit {
         }
     }
     feedback() {
-        console.log(this.rollno)
-        if (this.t1 >= 8 && this.t1 < 12) {
-            this.yesterday = new Date(this.time.setDate(this.time.getDate() - 1))
-            this.date = this.datePipe.transform(this.yesterday, "y-MMMM-d")
-            console.log("for dinner feedback date", this.date)
+        if (this.value !== undefined) {
+            console.log(this.rollno)
+            if (this.t1 >= 8 && this.t1 < 12) {
+                this.yesterday = new Date(this.time.setDate(this.time.getDate() - 1))
+                this.date = this.datePipe.transform(this.yesterday, "y-MMMM-d")
+                console.log("for dinner feedback date", this.date)
 
-        }
-        let result = Number(this.value)
-        console.log(result)
-        console.log(this.date)
-        let dialogRef = this.dialog.open(
-            ConfirmDialogComponent, { data: { duration: this.duration, result: result } }
-        );
-        dialogRef.afterClosed().subscribe(result => {
-            if (result) {
-                let obj = { "id": 1, "method": "UserService.FeedBack", "params": [{ "rollno": this.rollno, "time": this.date, "value": result }] };
-                this.appService.getPost(obj).subscribe(data => {
-                    console.log(data)
-                })
             }
-        });
+            let result = Number(this.value)
+            console.log(result)
+            console.log(this.date)
+            let dialogRef = this.dialog.open(
+                ConfirmDialogComponent, { data: { duration: this.duration, result: result } }
+            );
+            dialogRef.afterClosed().subscribe(result => {
+                if (result) {
+                    let obj = { "id": 1, "method": "UserService.FeedBack", "params": [{ "rollno": this.rollno, "time": this.date, "value": result }] };
+                    this.appService.getPost(obj).subscribe(data => {
+                        console.log(data)
+                    })
+                }
+            });
+        } else {
+            this.toaster.warning("Give ratting")
+        }
     }
 }
 
